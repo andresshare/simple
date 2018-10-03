4bb2bab6ddda6a3fd2ec782c4f3718300271bcbf
andresshare <andresshare@gmail.com>
2018-10-03 13:29:44 -0500

    models

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics')
+    stater = models.ForeignKey(User, related_name='topics')
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='post')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User,related_name='post')
+    update_by = models.ForeignKey(User, null=True, related_name='+')
